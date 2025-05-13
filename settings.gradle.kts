ed962067820e876eb97227a78e265b54155a4d2f
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2025-05-13 07:14:48 +0100

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "utils"
+rootProject.buildFileName = "engine.shared.utils.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        includeBuild("ServerDrivenUI")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
